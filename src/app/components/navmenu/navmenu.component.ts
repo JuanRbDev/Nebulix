3dc5f8323df6432fff2e427c314ae9d7550cae2f
JuanDev <juanrodriguezrbdev@gmail.com>
2024-10-26 00:32:05 -0500

    Guardar cambios antes de cambiar de rama

--- src/app/components/navmenu/navmenu.component.ts
+++ src/app/components/navmenu/navmenu.component.ts
@@ -1,12 +1,31 @@
 
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navmenu',
   standalone: true,
-  imports: [RouterModule], 
+  imports: [RouterModule],
   templateUrl: './navmenu.component.html',
   styleUrls: ['./navmenu.component.css']
 })
-export class NavmenuComponent { }
+export class NavmenuComponent {
+
+  menuOpen: boolean = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // Detectar clics fuera del menú
+  @HostListener('document:click', ['$event'])
+  closeMenu(event: Event) {
+    const target = event.target as HTMLElement;
+    const isOutsideClick = !target.closest('.navmenu') && !target.closest('.menu-icon');
+    
+    if (isOutsideClick && this.menuOpen) {
+      this.menuOpen = false;
+    }
+  }
+
+}
